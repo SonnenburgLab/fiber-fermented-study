b1798a27b45128f6f7e9b5e95527b3a142d06544
Gabriela Fragiadakis <gabrielafragiadakis@Gabrielas-MacBook-Pro.local>
2019-08-02 12:28:02 -0700

    initialized cytof data cleaning

---
+++ R/cytof_cleaning.R
@@ -0,0 +1,7 @@
+# This script cleans up the output exported from immuneatlas.org including:
+# * event counts (for frequencies)
+# * medians (for steady-state signaling)
+# the version of the exported data used for the publication's analysis can be found in the stated data directory
+
+data_directory <- "data/CyTOF/exported/"
+
