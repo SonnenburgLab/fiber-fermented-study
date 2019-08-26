32763a27a1c6475d0f546ef657c8392ee7b3a0ab
Gabriela Fragiadakis <gabrielafragiadakis@DN2tevso.SUNet>
2019-08-26 13:18:12 -0700

    add feature key function

--- R/combined_immune/create_feature_key.R
+++ R/combined_immune/create_feature_key.R
@@ -6,13 +6,37 @@ library(tidyverse)
 
 olink_differences <- read_csv("data/Olink/cleaned/Olink_data_differences_with_imputed.csv")
 
+cytof_signaling_differences <- read_csv("data/CyTOF/cleaned/cytof_signaling_differences_with_imputed.csv")
+
+cytof_frequency_differences <- read_csv("data/CyTOF/cleaned/cytof_frequency_differences_with_imputed.csv")
+
+# need phosphoflow ones, still redoing
+
+
 
 make_feature_key <- function(data, feature_type){
   
   gathered_data <- gather(data, key = "Feature", value = "value", -c(Participant, Group)) %>% select(Feature)
   
   feature_data <- tibble(Feature = gathered_data$Feature, Type = feature_type) %>%
-                    separate(col = Feature, into = c("Master_feature", "Time"), sep = "_T1_", remove = FALSE) 
+                    separate(col = Feature, into = c("Master_feature", "Time"), sep = "_T1_", remove = FALSE) %>%
+                    select(Feature, Master_feature, Time, Type)
                   
   
 }
+
+feature_sets <- list("Cytokine" = olink_differences, "Cell frequencies" = cytof_frequency_differences, "Endogenous signaling" = cytof_signaling_differences)
+
+type_list <- list()
+
+for (feature_type in names(feature_sets)){
+  
+  feature_info <- make_feature_key(data = feature_sets[[feature_type]], feature_type = feature_type)
+  
+  type_list[[feature_type]] <- feature_info
+  
+}
+
+feature_key <- reduce(type_list, bind_rows)
+
+# all that's left is to include phosphoflow and write the feature key to file
